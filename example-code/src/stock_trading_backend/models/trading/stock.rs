b8dc65da9c87a3e1f5d90e499c77327baf3115c6
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2023-12-19 03:14:34 -0500

    ⚡pushing -> `pushing changes 12-19-23` | pushing PR..

--- example-code/src/stock_trading_backend/models/trading/stock.rs
+++ example-code/src/stock_trading_backend/models/trading/stock.rs
@@ -2,8 +2,59 @@
 // ___________________________________________________________
 
 use serde::{Deserialize, Serialize};
+
+use utility_lib::iso_currency::Currency;
+use utility_lib::utils::utilities::format_price_with_currency_str;
 // ___________________________________________________________
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Stock {}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Stock {
+  pub symbol: String,
+  pub name: String,
+  pub current_price: f64,
+  pub formatted_current_price: String,
+}
 // ___________________________________________________________
+
+impl Stock {
+  pub fn new<Str: Into<String>>(symbol: Str, name: Str, current_price: f64) -> Self {
+    Self {
+      symbol: symbol.into(),
+      name: name.into(),
+      current_price,
+      formatted_current_price: format_price_with_currency_str(
+        &Currency::USD, // using `USD->$` currency symbol
+        format!("{:.2}", current_price), // converted `price` from `f64` to a `String`
+      ),
+    }
+  }
+
+  // /// Creates a new `Stock` instance wrapped in an `Arc`
+  // /// for thread-safe shared access in web applications.
+  // /// Use this for efficient concurrent handling of
+  // /// `Stock` data without cloning for each request.
+  // ///
+  // /// # Arguments
+  // /// * `symbol` - The stock symbol.
+  // /// * `name` - The company name.
+  // /// * `current_price` - The current stock price.
+  // ///
+  // /// # Returns
+  // /// An `Arc<Stock>` instance.
+  // pub fn arc_stock<Str: Into<String>>(symbol: Str, name: Str, current_price: f64) -> Arc<Stock> {
+  //   Arc::new(Stock::new(symbol, name, current_price))
+  // }
+}
+// ___________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
--- example-code/src/stock_trading_backend/stock_app.rs
+++ example-code/src/stock_trading_backend/stock_app.rs
@@ -2,14 +2,16 @@
 // ________________________________________________________
 
 use std::io::Error;
-use utility_lib::utils::utilities::{format_print, to_json_with_prefix, Void};
-use crate::models::trade_account::TradeAccount;
 
-use crate::models::user::User;
+use utility_lib::utils::utilities::{code_spacer, format_print, to_json_with_prefix, Void};
+
+use crate::models::account::user::User;
+use crate::models::trading::order::{Order, OrderStatus};
+use crate::models::trading::stock::Stock;
+use crate::models::trading::trade_account::TradeAccount;
 
 mod models;
 // __________________________________________________
-
 
 fn main() -> Result<Void, Error> {
   format_print("_", 45);
@@ -24,13 +26,79 @@ fn main() -> Result<Void, Error> {
   );
 
   to_json_with_prefix("Testing User object", &mock_user)?;
-  format_print("*", 45);
-  println!();
+  code_spacer(); // *******************************************************
 
   let mock_trade_acc = TradeAccount::new(1234567);
   to_json_with_prefix("Testing Trade Account object", &mock_trade_acc)?;
+  code_spacer(); // *******************************************************
+
+  let stocks = vec![
+    Stock::new("AAPL", "Apple Inc.", 150.00),
+    Stock::new("MSFT", "Microsoft Corporation", 250.00),
+    Stock::new("GOOGL", "Alphabet Inc.", 2700.00),
+    Stock::new("AMZN", "Amazon.com, Inc.", 3300.00),
+  ];
+
+  stocks.iter().for_each(|stock: &Stock| {
+    match to_json_with_prefix("Stock", stock) {
+      Ok(_) => {} // the function itself prints the result
+      Err(err) => eprintln!("Error converting stock to json: {}", err)
+    }
+  });
+  code_spacer(); // *******************************************************
+
+  // Start with a Buy order
+  let order_type = OrderStatus::Buy;
+
+  stocks.iter().for_each(|stock: &Stock| {
+    // Determine the quantity based on the order type
+    let quantity = match order_type {
+      OrderStatus::Buy => 10,
+      OrderStatus::Sell => 5,
+      OrderStatus::Pending => {
+        println!("Pending orders do not have a specified quantity yet.");
+        return; // Skip further processing for this stock
+      }
+      OrderStatus::Completed => {
+        println!("Completed orders do not change quantity.");
+        return; // Skip further processing for this stock
+      }
+    };
+
+    let price_per_share: &f64 = &stock.current_price;
+
+    // Create a new order with the specified details
+    let mut order = Order::new(
+      1,
+      stock.clone(), // Stock impl the `Clone` trait
+      quantity,
+      price_per_share.to_owned(),
+    );
+
+    format_print("=", 33);
+    println!("Before completion:");
+
+    // should display a msg stating no orders made
+    order.display_details();
+    // Now complete the order to display the results
+    order.complete_order();
+
+    println!("\nAfter completion:");
+    order.display_details();
+  });
+  code_spacer(); // *******************************************************
   // __________________________________________________
   format_print("_", 45);
   Ok(())
 }
 // __________________________________________________________
+
+
+
+
+
+
+
+
+
+
